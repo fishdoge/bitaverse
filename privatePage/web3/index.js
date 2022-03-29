00c2fac7ec033b181c868c0a6ff55ce68e3f7167
unknown <ru19wu06@gmail.com>
2022-03-30 01:50:25 +0800

    bitaverse finish

--- privatePage/web3/index.js
+++ privatePage/web3/index.js
@@ -7,13 +7,76 @@ async function setNFT_ABI(){
 
     bitaverse = await new web3.eth.Contract(BitaverseABI,Bitaverse_contract);
 
+    let nums = await bitaverse.methods.SpecialNFT().call();
+
+    $("#RareRemained").text(60 - nums);
+
+    if(60 - nums == 0){
+        $("#raremint").text("贈送完畢")
+    }
+
+    let normalR = await bitaverse.methods.PartnerOwned(coinbase).call();
+    console.log(normalR);
+    console.log(normalR.Nomal);
+    //NormalRemained
+    $("#RareRemained").text(normalR.Special);
+    $("#NormalRemained").text(normalR.Nomal);
+
 
 }
-
 setNFT_ABI();
 
 
+
+
 async function MintNFT(){
-    await bitaverse.methods.mint().send({from:coinbase,value:web3.utils.toWei('0.088','ether')});
+    let mintAmount = await bitaverse.methods.total_Mint().call();
+    console.log(mintAmount);
+    if(mintAmount < 10000){
+        await bitaverse.methods.mint().send({from:coinbase,value:web3.utils.toWei('0.088','ether')});
+    }else{
+        return;
+    }
+}
+
+async function WhiteListMint(){
+
+    let check = await bitaverse.methods.PartnerOwned(coinbase).call();
+
+
+
+    if(check>0){
+        await bitaverse.methods.WhiteListMints().send({from:coinbase});
+    }else{
+        alert("抱歉，你不在白名單內");
+    }
+}
+
+
+async function AdminMint(){
+    let normalR = await bitaverse.methods.PartnerOwned(coinbase).call();
+    console.log(normalR.Nomal);
+
+    let input_address = $("#freeInput").val();
+    if(normalR.Nomal>0){
+     await bitaverse.methods.SponsorAmount(input_address).send({from:coinbase});
+     window.location.reload();
+    }else{
+        alert("額度使用完畢");
+    }
+
+}
+
+async function AdminMintRare(){
+    let normalR = await bitaverse.methods.PartnerOwned(coinbase).call();
+    let input_address = $("#rareinput").val();
+
+
+    if(normalR.Special>0){
+    await bitaverse.methods.RareNftMint(input_address).send({from:coinbase});
+    window.location.reload();
+    }else{
+        alert("額度使用完畢");
+    }
 
 }
